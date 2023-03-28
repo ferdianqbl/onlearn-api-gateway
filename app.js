42df77a934da3d52261e72fcc6246927ff07c446
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-03-28 18:53:53 +0700

    initial project

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+// routes
+const coursesRouter = require("./app/courses/routes");
+const mediaRouter = require("./app/media/routes");
+const ordersRouter = require("./app/orders/routes");
+const paymentsRouter = require("./app/payments/routes");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// url routes
+app.use("/courses", coursesRouter);
+app.use("/media", mediaRouter);
+app.use("/orders", ordersRouter);
+app.use("/payments", paymentsRouter);
 
 module.exports = app;

f3c9b54d36fb5ee6dffa18009f704b38a179390e
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-04-04 21:44:53 +0700

    refactor call all handler and create getAll image

--- app/media/routes.js
+++ app/media/routes.js
@@ -1,10 +1,8 @@
 var express = require("express");
-const create = require("./handler/create");
+const { getAll, create } = require("./handler");
 var router = express.Router();
 
-// router.get("/", function (req, res, next) {
-//   res.send("respond with a resource");
-// });
+router.get("/", getAll);
 router.post("/", create);
 
 module.exports = router;

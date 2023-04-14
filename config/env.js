cd74af3f459a4047dc2d274d6eb3b89a3da8f19f
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-04-14 22:07:31 +0700

    create new env

--- config/env.js
+++ config/env.js
@@ -5,4 +5,10 @@ module.exports = {
   axios_timeout: process.env.AXIOS_TIMEOUT,
   url_service_media: process.env.URL_SERVICE_MEDIA || "localhost:8080",
   url_service_user: process.env.URL_SERVICE_USER || "localhost:5000",
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    secret_refresh_token: process.env.JWT_SECRET_REFRESH_TOKEN,
+    access_token_expired: process.env.JWT_ACCESS_TOKEN_EXPIRED,
+    refresh_token_expired: process.env.JWT_REFRESH_TOKEN_EXPIRED,
+  },
 };

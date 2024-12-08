95cfbf7a2a18e56b7122f75a7130f2a0fe06fc6c
ferdianqbl <muhammadferdianiqbal@gmail.com>
2024-12-08 23:33:30 +0700

    feat: api get mentor

--- app/mentors/routes.js
+++ app/mentors/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const getAll = require("./handler/getAll");
+const get = require("./handler/get");
 const router = express.Router();
 
 router.get("/", getAll);
+router.get("/:id", get);
 
 module.exports = router;

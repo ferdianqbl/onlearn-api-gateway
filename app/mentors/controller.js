c7fdf46a71b6ce4379891c2607ae19aa98c477c0
ferdianqbl <muhammadferdianiqbal@gmail.com>
2024-12-15 13:08:02 +0700

    feat: Courses Service -> Delete mentor

--- app/mentors/controller.js
+++ app/mentors/controller.js
@@ -1,4 +1,5 @@
 const create = require("./handler/create");
+const destroy = require("./handler/destroy");
 const get = require("./handler/get");
 const getAll = require("./handler/getAll");
 const update = require("./handler/update");
@@ -8,5 +9,5 @@ module.exports = {
   getAll,
   get,
   update,
-  // destroy,
+  destroy,
 };
--- app/mentors/routes.js
+++ app/mentors/routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getAll, get, create, update } = require("./controller");
+const { getAll, get, create, update, destroy } = require("./controller");
 const auth = require("../../middleware/auth");
 
 router.get("/", getAll);
 router.get("/:id", get);
 router.post("/", auth, create);
 router.put("/:id", auth, update);
+router.delete("/:id", auth, destroy);
 
 module.exports = router;
